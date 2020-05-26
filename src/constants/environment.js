4b68a0ac7bc5f8a4a145d3170a0e30d454ae975c
TechSquad404 <TechSquad404@gmail.com>
2020-05-26 15:23:37 +0530

    environment updated

--- src/constants/environment.js
+++ src/constants/environment.js
@@ -1,10 +1,10 @@
 const ENVIRONMENT = {
   development: {
-    BASE_URL: 'http://localhost:3000/',
+    BASE_URL: 'https://5pwc4.csb.app/',
     EVENT_LIST_URL: "data/events.json"
   },
   production: {
-    BASE_URL: 'https://5pwc4.csb.app/',
+    BASE_URL: 'http://localhost:3000/',
     EVENT_LIST_URL: "data/events.json"
   }
 };
